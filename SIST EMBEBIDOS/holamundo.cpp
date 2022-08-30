19bb70d8d82d6d33b063fb770b94168594895891
EmmanuelAnguiano <canguiano12@ucol.mx>
2022-08-30 08:15:17 -0500

    facu

---
+++ SIST EMBEBIDOS/holamundo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class holaMundo
+{
+    public:
+        void saludo()
+        {
+            cout << "Hello World!";
+        }
+};
+
+int main() {
+    holaMundo Metodo;
+    Metodo.saludo();
+    return 0;
+}
---
+++ SIST EMBEBIDOS/p1.cpp
@@ -0,0 +1,26 @@
+#include <iostream> //libreria para funciones de entrada y salida de flujo de info
+#define TEST 10
+
+//declaraciones estandar de la consola
+using std::cout; //salida de texto por consola
+using std::endl; //salto de linea en consola
+using std::cin; //ingreso de texto por consola
+/*
+//punto de inicio de ejecucion 
+void mi_primer_funcion (void);
+int main(void){ //nucelo del programa
+	cout <<"el area es" <<endl;
+	cout << TEST <<endl;
+	//system("pause");     //el programa temrina hasta que termina la tecla
+	cin.get();
+	
+	return 0;
+}*/
+
+void mi_primer(void){
+	cout<<"hola hola hola hola hola"<<endl;
+}
+int main(void){
+	mi_primer();
+	cin.get();;
+}
