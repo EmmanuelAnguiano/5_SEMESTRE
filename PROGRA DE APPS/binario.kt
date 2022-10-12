f74c608c1b695bfc68c5051e7bc4517196d23949
EmmanuelAnguiano <canguiano12@ucol.mx>
2022-10-12 12:41:39 -0500

    facultad

---
+++ PROGRA DE APPS/binario.kt
@@ -0,0 +1,20 @@
+
+
+
+/*
+Decimal a binario
+fun main(){
+    var res = mutableListOf<Int>()
+    println("dame un valor a convertir: ")
+    var x=readLine()!!.toInt()
+    while(x>=1){
+        println(x)
+        if(x%2==0){
+            res.add(0)
+        }else{
+            res.add(1)
+        }
+        x=x/2
+    }
+    println(res.reversed())
+}*/
