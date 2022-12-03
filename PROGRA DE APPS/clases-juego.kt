1aa4ddea5a637672ce2236a974de2e041041f05b
EmmanuelAnguiano <canguiano12@ucol.mx>
2022-12-03 13:11:19 -0600

    facultad

---
+++ PROGRA DE APPS/clases-juego.kt
@@ -0,0 +1,11 @@
+abstract class peleador{
+    var nombre: String = "peleadorX";
+    var nivel_sangre:: Int = 100;
+    var nivel_danio_golpe: Int = 0;
+    var nivel_danio_especial: Int = 0;
+    var nivel_proteccion: Int = 0;
+    var estatura: Int = 0;
+    var peso: Int = 0;
+    var esta_protegiendose: Boolean = false;
+}
+
